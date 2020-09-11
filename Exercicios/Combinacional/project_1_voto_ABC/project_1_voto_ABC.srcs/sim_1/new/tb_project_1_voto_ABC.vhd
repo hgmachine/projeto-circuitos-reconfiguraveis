79d34d9f0f41c541b8ffd89fffadc151d05a0c94
Hgmachine <hgmoura@yahoo.com>
2020-09-10 22:38:57 -0300

    First project: voto ABC

---
+++ Exercicios/Combinacional/project_1_voto_ABC/project_1_voto_ABC.srcs/sim_1/new/tb_project_1_voto_ABC.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tb_project_1_voto_ABC is
+
+end tb_project_1_voto_ABC;
+
+architecture Behavioral of tb_project_1_voto_ABC is
+
+    component project_1_voto_ABC is
+        Port ( a : in STD_LOGIC;
+               b : in STD_LOGIC;
+               c : in STD_LOGIC;
+               f : out STD_LOGIC);
+    end component;
+
+    signal s_a, s_b, s_c : std_logic := '0';
+    signal s_f : std_logic := '0'; 
+
+begin
+
+     uut : project_1_voto_ABC port map (
+         a => s_a,
+         b => s_b,
+         c => s_c,
+         f => s_f
+     );
+
+     s_a <= '0', '1' after 20 ns, '0' after 40 ns, '0' after 40 ns, '1' after 80 ns;
+     s_b <= '0', '1' after 20 ns, '1' after 40 ns, '1' after 40 ns, '1' after 80 ns;
+     s_c <= '0', '0' after 20 ns, '0' after 40 ns, '1' after 40 ns, '1' after 80 ns;
+       
+end Behavioral;
